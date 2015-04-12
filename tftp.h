f6920c2d7237117134c6f789ba7450bcbc972e16
Cristian Rodriguez <cfrodca@gmail.com>
2015-04-12 00:10:36 -0500

    Update tftp.h

--- tftp.h
+++ tftp.h
@@ -11,12 +11,26 @@
 #include <ti/ndk/inc/netmain.h>
 #include <ti/ndk/inc/_stack.h>
 
-#define SEGSIZE   512     /* data segment size */
+/* Error codes. */
+typedef enum
+{
+	TFTP_ERR_NONE = 	  -1,			/* no error */
+	TFTP_ERR_NDEF =      0,       /* not defined */
+	TFTP_ERR_NOTFOUND =  1,       /* file not found */
+	TFTP_ERR_ACCESS =    2,       /* access violation */
+	TFTP_ERR_NOSPACE =   3,       /* disk full or allocation exceeded */
+	TFTP_ERR_BADOP =     4,       /* illegal TFTP operation */
+	TFTP_ERR_BADID =     5,       /* unknown transfer ID */
+	TFTP_ERR_EXISTS =    6,       /* file already exists */
+	TFTP_ERR_NOUSER =	   7,       /* no such user */
+} tTFTPError;
 
-#define PORT_TFTP 69
+#define SEGSIZE   		512     /* data segment size */
+
+#define PORT_TFTP 		69		  /* puerto tftp */
 
 int dtask_tftp( SOCKET s, UINT32 unused );
-typedef int (*tTFTPRequest)(char *fileName, char *buffer, UINT32 *bufferSize, UINT16 block);
+typedef tTFTPError (*tTFTPRequest)(char *fileName, char *buffer, UINT32 *bufferSize, UINT16 block);
 void tftpInit(tTFTPRequest pfnRequest);
 
 
